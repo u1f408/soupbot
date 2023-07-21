17f4be6e85fb7122fde4ca80c7a960510416d7b0
Iris System <iris@iris.ac.nz>
2023-07-21 20:42:01 +1200

    set status on bot ready

--- bot.rb
+++ bot.rb
@@ -13,11 +13,16 @@ class SoupBot
     b.call(self.bot)
   end
 
+  def self.on_ready(ev)
+    self.bot.update_status(:online, "to contented purring", nil, nil, false, 2)
+  end
+
   def self.run
     Dir.glob(File.join(File.expand_path('../commands', __FILE__), '*.rb')) do |f|
       require f
     end
 
+    self.bot.ready(&method(:on_ready))
     self.bot.run
   end
 end
